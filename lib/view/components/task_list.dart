e2327ac7dde1d2a0eb242b59008e6a7fddea0709
Michael Spiegel <michaelspiegel@live.at>
2023-12-28 20:44:32 +0100

    Readded showing message if not tasks are found.

--- lib/view/components/task_list.dart
+++ lib/view/components/task_list.dart
@@ -20,101 +20,111 @@ class TaskList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      padding: const EdgeInsets.all(8),
-      itemCount: tasks.length,
-      itemBuilder: (BuildContext context, int index) {
-        var taskId = tasks[index].id;
-        var taskName = tasks[index].name;
-        var startTimestamp = tasks[index].startTimestamp;
-        var endTimestamp = tasks[index].endTimestamp;
-        var startTimestampStr = _formatDate(tasks[index].startTimestamp);
-        var endTimestampStr = _formatDate(tasks[index].endTimestamp);
-        var details = tasks[index].details;
+    if (tasks.isEmpty) {
+      return const Center(
+          child: Text(
+        "No tasks found",
+        style: TextStyle(
+            fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black),
+      ));
+    } else {
+      return ListView.separated(
+        padding: const EdgeInsets.all(8),
+        itemCount: tasks.length,
+        itemBuilder: (BuildContext context, int index) {
+          var taskId = tasks[index].id;
+          var taskName = tasks[index].name;
+          var startTimestamp = tasks[index].startTimestamp;
+          var endTimestamp = tasks[index].endTimestamp;
+          var startTimestampStr = _formatDate(tasks[index].startTimestamp);
+          var endTimestampStr = _formatDate(tasks[index].endTimestamp);
+          var details = tasks[index].details;
 
-        return Container(
-          margin: const EdgeInsets.all(5),
-          decoration: BoxDecoration(
-            color: Colors.yellow.shade300,
-            boxShadow: const [
-              BoxShadow(color: Colors.black12, blurRadius: 12, spreadRadius: 1),
-            ],
-          ),
-          child: Column(
-            children: [
-              ListTile(
-                contentPadding: const EdgeInsets.all(10),
-                title: Padding(
-                  padding: const EdgeInsets.only(bottom: 10),
-                  child: Text(
-                    taskName,
-                    style: const TextStyle(fontSize: 19),
+          return Container(
+            margin: const EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: Colors.yellow.shade300,
+              boxShadow: const [
+                BoxShadow(
+                    color: Colors.black12, blurRadius: 12, spreadRadius: 1),
+              ],
+            ),
+            child: Column(
+              children: [
+                ListTile(
+                  contentPadding: const EdgeInsets.all(10),
+                  title: Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: Text(
+                      taskName,
+                      style: const TextStyle(fontSize: 19),
+                    ),
+                  ),
+                  subtitle: Text(
+                    details,
+                    style: const TextStyle(fontSize: 17),
                   ),
                 ),
-                subtitle: Text(
-                  details,
-                  style: const TextStyle(fontSize: 17),
+                const Divider(
+                  color: Colors.black,
+                  thickness: 0.8,
                 ),
-              ),
-              const Divider(
-                color: Colors.black,
-                thickness: 0.8,
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 3, horizontal: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "$startTimestampStr - $endTimestampStr",
-                      style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.italic),
-                    ),
-                    Row(
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => CreateEditTaskPage(
-                                          taskId: taskId,
-                                          taskName: taskName,
-                                          startTimestamp: startTimestamp,
-                                          endTimestamp: endTimestamp,
-                                          details: details,
-                                        )));
-                          },
-                          child: const Icon(
-                            Icons.edit,
-                            size: 28,
-                            color: Colors.orange,
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 3, horizontal: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "$startTimestampStr - $endTimestampStr",
+                        style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            fontStyle: FontStyle.italic),
+                      ),
+                      Row(
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => CreateEditTaskPage(
+                                            taskId: taskId,
+                                            taskName: taskName,
+                                            startTimestamp: startTimestamp,
+                                            endTimestamp: endTimestamp,
+                                            details: details,
+                                          )));
+                            },
+                            child: const Icon(
+                              Icons.edit,
+                              size: 28,
+                              color: Colors.orange,
+                            ),
                           ),
-                        ),
-                        InkWell(
-                          onTap: () {
-                            onDeletePressedFunction(taskId);
-                            tasks.remove(tasks[index]);
-                          },
-                          child: const Icon(
-                            Icons.delete_forever,
-                            size: 28,
-                            color: Colors.red,
+                          InkWell(
+                            onTap: () {
+                              onDeletePressedFunction(taskId);
+                              tasks.remove(tasks[index]);
+                            },
+                            child: const Icon(
+                              Icons.delete_forever,
+                              size: 28,
+                              color: Colors.red,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        );
-      },
-      separatorBuilder: (BuildContext context, int index) => const Divider(),
-    );
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+      );
+    }
   }
 }
