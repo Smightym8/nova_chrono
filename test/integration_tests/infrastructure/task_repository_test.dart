82eab6525310cdaeed7909153bfd3d31018dae74
Michael Spiegel <michaelspiegel@live.at>
2024-01-03 18:27:19 +0100

    Implemented tests for update and delete task.

--- test/integration_tests/infrastructure/task_repository_test.dart
+++ test/integration_tests/infrastructure/task_repository_test.dart
@@ -58,5 +58,52 @@ void main() {
         expect(taskActual, taskExpected);
       }
     });
+
+    test("Given task when update then task is updated", () async {
+      // Given
+      var taskOriginal = Task(
+          "1",
+          "Task 1",
+          DateTime(2023, 1, 20, 14, 30),
+          DateTime(2023, 1, 20, 15, 00),
+          ""
+      );
+
+      var taskUpdated = Task(
+          "1",
+          "Task 1",
+          DateTime(2023, 1, 20, 13, 30),
+          DateTime(2023, 1, 20, 14, 00),
+          "Some details for task 1"
+      );
+
+      // When
+      await taskRepository.updateTask(taskUpdated);
+
+      // Then
+      var updatedTask = await taskRepository.getById(taskOriginal.id);
+      expect(updatedTask, isNot(null));
+      expect(updatedTask!.id, taskUpdated.id);
+      expect(updatedTask.name, taskUpdated.name);
+      expect(updatedTask.startTimestamp, taskUpdated.startTimestamp);
+      expect(updatedTask.endTimestamp, taskUpdated.endTimestamp);
+      expect(updatedTask.details, taskUpdated.details);
+
+      expect(updatedTask.startTimestamp, isNot(taskOriginal.startTimestamp));
+      expect(updatedTask.endTimestamp, isNot(taskOriginal.endTimestamp));
+      expect(updatedTask.details, isNot(taskOriginal.details));
+    });
+
+    test("Given taskId when delete then task is deleted", () async {
+      // Given
+      var taskId = "4";
+
+      // When
+      await taskRepository.deleteTask(taskId);
+
+      // Then
+      var deletedTask = await taskRepository.getById(taskId);
+      expect(deletedTask, null);
+    });
   });
 }
