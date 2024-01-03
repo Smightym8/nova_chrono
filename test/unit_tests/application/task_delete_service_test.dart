5e2d76585949dff7f43f6e0390b4d7cb0d24e193
Michael Spiegel <michaelspiegel@live.at>
2024-01-03 16:34:28 +0100

    Implemented test for TaskDeleteService.

---
+++ test/unit_tests/application/task_delete_service_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:nova_chrono/application/api/task_delete_service.dart';
+import 'package:nova_chrono/application/impl/task_delete_service_impl.dart';
+
+import '../../mocks/annotations.mocks.dart';
+
+void main() {
+  group("TaskDeleteService Tests", () {
+    late TaskDeleteService taskDeleteService;
+    late MockTaskRepository mockTaskRepository;
+
+    setUp(() {
+      mockTaskRepository = MockTaskRepository();
+      taskDeleteService = TaskDeleteServiceImpl(taskRepository: mockTaskRepository);
+    });
+
+    test("Given taskId when calling deleteTask then "
+        "mockTaskRepository.delete is called", () {
+      // Given
+      String taskId = "1";
+
+
+      // When
+      taskDeleteService.deleteTask(taskId);
+
+      // Then
+      verify(mockTaskRepository.deleteTask(taskId));
+    });
+  });
+}
