d48a789d339fa2b2b83104bc699a2115891776b3
Michael Spiegel <michaelspiegel@live.at>
2024-01-07 21:46:59 +0100

    Changed widget integration test to pump whole app and to initialize IntegrationTestWidgetsFlutterBinding.

--- test/integration_tests/view/delete_task_flow_test.dart
+++ test/integration_tests/view/delete_task_flow_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 import 'package:nova_chrono/application/api/task_create_service.dart';
 import 'package:nova_chrono/application/api/task_delete_service.dart';
 import 'package:nova_chrono/application/api/task_edit_service.dart';
@@ -10,13 +11,15 @@ import 'package:nova_chrono/application/impl/task_edit_service_impl.dart';
 import 'package:nova_chrono/application/impl/task_list_service_impl.dart';
 import 'package:nova_chrono/domain/repository/task_repository.dart';
 import 'package:nova_chrono/infrastructure/task_repository_impl.dart';
-import 'package:nova_chrono/view/pages/home_page.dart';
+import 'package:nova_chrono/main.dart';
 import 'package:nova_chrono/view/providers/task_filter_date_provider.dart';
 import 'package:provider/provider.dart';
 
 import '../../utils/test_database_provider.dart';
 
 void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
   group("Deleting task flow test", () {
     late TestDatabaseProvider testDatabaseProvider;
     late TaskRepository taskRepository;
@@ -41,27 +44,21 @@ void main() {
       await testDatabaseProvider.deleteDatabase();
     });
 
-    testWidgets("Give at leas one task in the task list when deleting a task "
+    testWidgets("Given at leas one task in the task list when deleting a task "
         "the task is not in the list anymore", (tester) async {
       await tester.runAsync(() async {
         await tester.pumpWidget(
-            MultiProvider(
-              providers: [
-                ChangeNotifierProvider(create: (context) => TaskFilterDateProvider()),
-              ],
-              child: Directionality(
-                textDirection: TextDirection.ltr,
-                child: MaterialApp(
-                  home: HomePage(
-                    title: "Title",
-                    taskCreateService: taskCreateService,
-                    taskListService: taskListService,
-                    taskEditService: taskEditService,
-                    taskDeleteService: taskDeleteService,
-                  ),
-                ),
-              ),
-            )
+          MultiProvider(
+            providers: [
+              ChangeNotifierProvider(create: (context) => TaskFilterDateProvider()),
+            ],
+            child: App(
+              taskCreateService: taskCreateService,
+              taskListService: taskListService,
+              taskEditService: taskEditService,
+              taskDeleteService: taskDeleteService,
+            ),
+          ),
         );
 
         await Future.delayed(const Duration(seconds: 1));
